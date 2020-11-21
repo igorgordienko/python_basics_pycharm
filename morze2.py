47aa674345af962bdabc46101b0c65cc4d78b470
igorgord <igorgordienko2@gmail>
2020-11-20 19:21:54 -0800

    refactoring of morze2.py

--- morze2.py
+++ morze2.py
@@ -26,25 +26,17 @@ def decode_morse(morse_code):
 
     list_of_words = morse_code.split('   ')
     final_list = []
-    print("this is list_of_word: {}".format(list_of_words))
 
     for word in list_of_words:
         decoded_word = ""
 
-        #   print("This is word: {}".format(word))
-
         list_of_chars = word.split(' ')
 
-        #  print("This is list of chars: {}".format(list_of_chars))
-
         for letter in list_of_chars:
-            #   print("This is char: {}".format(char))
-            #  print("This is decoded: {}".format(get_key(char)))
             decoded_word += get_key(letter)
             print(decoded_word)
         final_list.append(decoded_word)
 
-    print(' '.join(final_list))
     return ' '.join(final_list)
 
 
